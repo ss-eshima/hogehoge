1a040c01756e10817d4527515fa4651c968da4d1
ss-eshima <hirokazu.eshima@supership.jp>
2018-09-18 20:07:21 +0900

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,17 @@
+WORKDIR=/tmp
+WORKFILE=$WORKDIR/.work
+
+cat <<'EOF' > $WORKDIR/$APP_NAME/files/temp.sh
+
+echo "curl -s -d ' {\""auth\"":null,\""method\"":\""user.login\"",\""id\"":1,\""params\"":{\""password\"":\""`echo $JENKINS_INJECTS_ZABBIX_USER_PASSWORD`\"",\""user\"":\""`echo $JENKINS_INJECTS_ZABBIX_USER_NAME`\""},\""jsonrpc\"":\""2.0\""}' -H \""Content-Type: application/json-rpc\"" http://da-zabbix2.scaleout.local/zabbix/api_jsonrpc.php" | sh | jq -r '.result' > /tmp/token && token=`cat /tmp/token`
+echo "curl -s -d ' {\""auth\"":\""`echo $token`\"",\""method\"":\""trigger.get\"",\""id\"":1,\""params\"":{\""output\"":\""extend\"",  \""expandDescription\"":1,  \""monitored\"":1,  \""filter\"":{\""status\"":0,\""value\"":1} },\""jsonrpc\"":\""2.0\""} ' -H \""Content-Type: application/json-rpc\"" http://da-zabbix2.scaleout.local/zabbix/api_jsonrpc.php" | sh |  jq -r '.result[].description' | sort | nl
+
+sed -i '1s/^/\`\`\`\n/' $WORKFILE
+sed -i -e '$ a \`\`\`' $WORKFILE
+
+curl -k -S $JENKINS_INJECTS_GO_SCRIPT_URL > $WORKDIR/slack-notify.go
+go run $WORKDIR/slack-notify.go \
+-f $WORKFILE \
+-u "$JENKINS_INJECTS_SLACK_USER" \
+-r "$JENKINS_INJECTS_SLACK_ROOM" \
+-d "$JENKINS_INJECTS_SLACK_URL"
