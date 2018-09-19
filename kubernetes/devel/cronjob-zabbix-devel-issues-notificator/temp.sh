b0eb992be79bf8843784b7a7e36de9d3660b75a2
ss-eshima <hirokazu.eshima@supership.jp>
2018-09-19 12:06:50 +0900

    Update temp.sh

--- kubernetes/devel/cronjob-zabbix-devel-issues-notificator/temp.sh
+++ kubernetes/devel/cronjob-zabbix-devel-issues-notificator/temp.sh
@@ -1,17 +1,14 @@
-WORKDIR=___CONTAINER_WORKDIR___
-WORKFILE=___CONTAINER_WORKFILE___
+cat <<'EOF' > $CONTAINER_WORKDIR_SET_BY_JENKINS/$APP_NAME_SET_BY_JENKINS/temp.sh
 
-cat <<'EOF' > $WORKDIR/$APP_NAME/temp.sh
-
-echo "curl -s -d ' {\""auth\"":null,\""method\"":\""user.login\"",\""id\"":1,\""params\"":{\""password\"":\""`echo $JENKINS_INJECTS_ZABBIX_USER_PASSWORD`\"",\""user\"":\""`echo $JENKINS_INJECTS_ZABBIX_USER_NAME`\""},\""jsonrpc\"":\""2.0\""}' -H \""Content-Type: application/json-rpc\"" http://da-zabbix2.scaleout.local/zabbix/api_jsonrpc.php" | sh | jq -r '.result' > $WORKDIR/.token && token=`cat $WORKDIR/.token`
+echo "curl -s -d ' {\""auth\"":null,\""method\"":\""user.login\"",\""id\"":1,\""params\"":{\""password\"":\""`echo $ZABBIX_USER_PASSWORD_SET_BY_JENKINS`\"",\""user\"":\""`echo $ZABBIX_USER_NAME_SET_BY_JENKINS`\""},\""jsonrpc\"":\""2.0\""}' -H \""Content-Type: application/json-rpc\"" http://$ZABBIX_API_HOST_SET_BY_JENKINS/zabbix/api_jsonrpc.php" | sh | jq -r '.result' > $CONTAINER_WORKDIR_SET_BY_JENKINS/.token && token=`cat $CONTAINER_WORKDIR_SET_BY_JENKINS/.token`
 echo "curl -s -d ' {\""auth\"":\""`echo $token`\"",\""method\"":\""trigger.get\"",\""id\"":1,\""params\"":{\""output\"":\""extend\"",  \""expandDescription\"":1,  \""monitored\"":1,  \""filter\"":{\""status\"":0,\""value\"":1} },\""jsonrpc\"":\""2.0\""} ' -H \""Content-Type: application/json-rpc\"" http://$JENKINS_INJECTS_ZABBIX_HOST/zabbix/api_jsonrpc.php" | sh |  jq -r '.result[].description' | sort | nl
 
-sed -i '1s/^/\`\`\`\n/' $WORKFILE
-sed -i -e '$ a \`\`\`' $WORKFILE
+sed -i '1s/^/\`\`\`\n/' $CONTAINER_WORKFILE_SET_BY_JENKINS
+sed -i -e '$ a \`\`\`' $CONTAINER_WORKFILE_SET_BY_JENKINS
 
-curl -k -S $JENKINS_INJECTS_GO_SCRIPT_URL > $WORKDIR/slack-notify.go
-go run $WORKDIR/slack-notify.go \
--f $WORKFILE \
--u "$JENKINS_INJECTS_SLACK_USER" \
--r "$JENKINS_INJECTS_SLACK_ROOM" \
--d "$JENKINS_INJECTS_SLACK_URL"
+curl -k -S $GO_SCRIPT_URL_SET_BY_JENKINS > $CONTAINER_WORKDIR_SET_BY_JENKINS/slack-notify.go
+go run $CONTAINER_WORKDIR_SET_BY_JENKINS/slack-notify.go \
+-f $CONTAINER_WORKFILE_SET_BY_JENKINS \
+-u "$SLACK_USER_SET_BY_JENKINS" \
+-r "$SLACK_ROOM_SET_BY_JENKINS" \
+-d "$SLACK_URL_SET_BY_JENKINS"
